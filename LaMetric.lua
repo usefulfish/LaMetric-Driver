9660066aada6d3fc12a40643a44ee47f7afebc65
Your Name <you@example.com>
2024-11-23 09:20:42 +0000

    Spelling!

--- LaMetric.lua
+++ LaMetric.lua
@@ -1,19 +1,19 @@
 -- Driver definitions
 driver_label = "LaMetric"
-driver_load_system_help = "Allows integration with LaMetric Time device."
+driver_load_system_help = "Allows integration with a LaMetric Time device."
 driver_has_capture = false
 driver_help = [[
-## LaMetric TIME
+## LaMetric Time
 
-The primamy usage would be to display notifications, but resoures are also available which allow interaction
-with the various built\-in apps.
+The primary usage would be to display notifications, but resources are also available which allow interaction
+with the various native apps.
 
-### Connection to the TIME device
+### Connection to the Time device
 
-You will need to add one instance of the driver per LaMetric TIME device. Once connected, each driver instacne
-will talk to a single device and expose apps installed via resource discovery.
+You will need to add one instance of the driver per LaMetric Time device. Once connected, each driver instance
+will talk to a single device and expose installed apps via resource discovery.
 
-The TIME device does not support setting a static IP address directly, you must either use
+The Time device does not support setting a static IP address directly, you must either use
 the advertised network name or an IP address you assign. To find the network name use the smartphone app and select
 *Wi\-Fi* under *Settings* and the name is visible under *Local Domain*. Alternatively assign the device a network name
 or static IP using your router.
@@ -22,57 +22,57 @@ Connection settings are:
 
 - **La metric time api key**: This can be obtained by accessing the settings for the device from the smartphone
   app and requesting one under *Device API Key*.
-- **La metric time host**: IP address or network name of the LaMetric TIME device (see note above).
-- **Poll interval**: The number of seconds betwwen polling for resource state updates. Probably
+- **La metric time host**: IP address or network name of the LaMetric Time device.
+- **Poll interval**: The number of seconds between polling for resource state updates. Probably
   should keep polling to a minimum.
 - **Check server certificate**: This should be turned off.
 
 ### Available Resources
 
-- **\_LAMETRIC TIME**: A LaMetric TIME device allowing notifications and access to basic settings.
+- **\_LAMETRIC TIME**: A LaMetric Time device allowing notifications and access to basic settings.
 
 This is the main resource for the device. It allows sending notifications, basic operation similar to the
-the buttins on the device itself, as well as control of some simple settings.
+the buttons on the device itself, as well as control of some simple settings.
 
-Access to the apps on the device is achieved though separate reources for each app. The supported app resource
+Access to the apps on the device is achieved though separate resources for each app. The supported app resource
 types are:
 
-- **\_CLOCK APP**: interaction with the built\-in alarm clock app.
-- **\_TIMER APP**: interaction with the built\-in countdown timer app.
-- **\_STOPWATCH APP**: interaction with the built\-in stopwatch app.
-- **\_RADIO APP**: interaction with the built\-in radio app.
-- **\_WEATHER APP**: interaction with the built\-in weather app.
-- **\_3RD PARTY APP**: interaction with a generic app from the LaMetric MARKET. No app specific commands are avaiable.
+- **\_CLOCK APP**: interaction with the native alarm clock app.
+- **\_TIMER APP**: interaction with the native countdown timer app.
+- **\_STOPWATCH APP**: interaction with the native stopwatch app.
+- **\_RADIO APP**: interaction with the native radio app.
+- **\_WEATHER APP**: interaction with the native weather app.
+- **\_3RD PARTY APP**: interaction with a generic app from the LaMetric Market. No app specific commands are available.
 
 ### Resource addresses and discovery
 
-Resource discovery is available and is required becuse of the way in which the TIME device
-allocates addresses to applications. The primary device and all installed apps will be listed
-for you to add to your probject. Only installed apps will show up, so if an app is removed from the
+Resource discovery is available and is required because of the way in which the Time device
+allocates addresses to applications. Once connection, the primary device and all installed apps will be listed
+for you to add to your project. Only installed apps will show up, so if an app is removed from the
 device then it will not be listed.
 
-The addresses for the TIME device and apps are populated by the resource discovery. For an app, the unique address
-can change if you delete the app from your TIME deviceand re\-install it \- in this case you would need
-to use the resource discovery again to find the new addres and then you can update it from there.
+The addresses for the Time device and apps are populated by the resource discovery. For an app, the unique address
+can change if you delete the app from your Time device and re\-install it \- in this case you would need
+to use the resource discovery again to find the new address and then you can update it from there.
 
 ### Resource Events
 
-The LaMetric TIME does not support events over its API, but the driver will poll For
+The LaMetric Time does not support events over its API, but the driver will poll For
 changes on the device and raise appropriate state update events as expected. Given that the primary
-usage of this drive would be to send notifications and some basic operation of the built\-in apps, then
+usage of this drive would be to send notifications and some basic operation of the native apps, then
 it is not expected that you would rely on these updates so polling should be kept to a minimum.
 
 ### Resource Commands
 
 #### Device Notifications
 
-The primary use case wold be to send notifications and alerts to be displayed on the TIME device. The device supports
-rich customisation of these notifications so we have exposed two commands for this: a basic one to easily send a simple
-notification to the device, and an enhanced one giving the full range of customisation.
+The primary use case would be to send notifications and alerts to be displayed on the Time device. The device supports
+rich customization of these notifications so we have exposed two commands for this: a basic one to easily send a simple
+notification to the device, and an enhanced one giving the full range of customization.
 
 *\_LAMETRIC TIME*
 
-- **\_NOTIFY**: Display a simple message on the TIME device.
+- **\_NOTIFY**: Display a simple message on the Time device.
     - *\_NOTIFICATION PRIORITY*: Controls how the notification is handled by the device.
       - INFO: This level of notification should be used for notifications like news, weather, temperature, etc. This
       notification will not be shown when screensaver is active.
@@ -83,7 +83,7 @@ notification to the device, and an enhanced one giving the full range of customi
       be used only for really important notifications like notifications from smoke detectors, water leak sensors, etc. Use
       it for events that require human interaction immediately.
     - *\_MESSAGE*: The text of the message to display.
-- **\_NOTIFY WITH DETAILS**: Display a customised message on the TIME device. Arguments are populated with defaults
+- **\_NOTIFY WITH DETAILS**: Display a customized message on the Time device. Arguments are populated with defaults
 which match the simple message command.
     - *\_MESSAGE*: The text of the message to display.
     - *\_MESSAGE ICON*: Icon id or base64 encoded binary. There is a huge array of possible options. Choose
@@ -94,7 +94,7 @@ which match the simple message command.
     - *\_NOTIFICATION PRIORITY*: As above.
     - *\_NOTIFICATION TYPE*: Indicates the nature of the notification. This defines an optional "announcing"
     icon which is displayed before the message is displayed.
-        - NONE: No notification preabble will be shown.
+        - NONE: No notification preamble will be shown.
         - INFO: "i" icon will be displayed prior to the notification. Means that notification contains information,
         no need to take actions on it.
         - ALERT: "!!!" icon will be displayed prior to the notification. Use it when you want the user to pay attention
@@ -102,7 +102,7 @@ which match the simple message command.
     - *\_SOUND CATEGORY*: Indicates the category of the sound id selected.
         - ALARMS: The sound id supplied should be one of the *alarm sound ids* listed below.
         - NOTIFICATIONS: The sound id supplied should be one of the *notification sound ids* listed below.
-    - *\_SOUND ID*: Should be one of the ids listed below for the above category e.g. alarm1 or dog etc.
+    - *\_SOUND ID*: Should be one of the ids listed below for the above category e.g. *alarm1* or *dog* etc.
     - *\_SOUND REPEAT*: Defines the number of times sound must be played. If set to 0 sound will be played until
     notification is dismissed.
 
@@ -121,38 +121,38 @@ statistic, thunder, water1, water2, win, win2, wind, wind\_short.
 The *\_LAMETRIC TIME* resource also supports the following app and settings commands:
 
 - *\_LAMETRIC TIME*
-    - **\_NEXT APPLICATION**: Make the next app visible.
-    - **\_PREV APPLICATION**: Make the previous app visible.
+    - **\_NEXT APPLICATION**: Make the next app visible. Equivalent to pressing the right button on the device.
+    - **\_PREV APPLICATION**: Make the previous app visible. Equivalent to pressing the left button on the device.
     - **\_SET MODE**: Set the app switching mode of the device. This might be required if you want a selected app to remain visible.
-        - *\_MODE*: The mode to select \- see resource states below.
+        - *\_MODE*: The app switching mode to set \- see resource states below.
     - **\_SET VOLUME**: Set the volume of the device. This will affect sounds played as part of a notification as well as the radio.
-      - *\_VOLUME*: The volume to set.
+      - *\_VOLUME*: The overall volume to set.
     - **\_SET SCREENSAVER**: Enables or disables the screensaver. This might be required because app switching
     might not work as expected when the screensaver is active.
         - *\_ENABLED*: True to enable the screensaver, false to disable it.
 
-#### Interaction with aps and app switching
+#### Interaction with apps and app switching
 
-You can switch forward and backwards between apps using commands on the TIME device reource. Alternativy you can select
-a specific app on the resource for that app by using its *\_ACTIVATE* command.
+You can switch forward and backwards between apps using the commands on the Time device resource. Alternatively you can select
+a specific app directly on the resource for that app by using its *\_ACTIVATE* command.
 
 Some apps have additional commands and these are detailed below.
 
 - \_CLOCK APP
     - **\_ACTIVATE**: Makes the clock the currently displayed app.
-    - **\_ENABLE ALARM**: Turn the alarm on or off. This will act on the *first* alarm set on the TIME device.
+    - **\_ENABLE ALARM**: Turn the alarm on or off. This will act on the *first* alarm set on the Time device.
         - *\_ENABLED*: Turns on the alarm if set to true, turns it off otherwise.
         - *\_WAKE WITH RADIO*: If true, radio will be activated when alarm goes off.
     - **\_SET ALARM**: Change the time and action of the alarm for a specified time. This command will create
-    a new alarm or mofidy an existing alarm if one already exists matching the supplied time.
+    a new alarm or modify an existing alarm if one already exists matching the supplied time.
         - *\_ENABLED*: Turns on the alarm if set to true, turns it off otherwise.
         - *\_TIME*: Local time in format "HH:mm" or "HH:mm:ss".
         - *\_WAKE WITH RADIO*: If true, radio will be activated when alarm goes off.
-    - **\_SET CLOCKFACE**: Select the type of icon dispayed when the clock is visible.
+    - **\_SET CLOCKFACE**: Select the type of icon displayed when the clock is visible.
         - *\_TYPE*:
             - NONE: No icon is displayed.
             - WEATHER: Displays an icon representing the current weather conditions for your area.
-            - PAGE\_A_\DAY: Displays a calander icon or an indicator for special days in your area.
+            - PAGE\_A_\DAY: Displays a calendar icon or an indicator for special days in your area.
 - \_TIMER APP
     - **\_ACTIVATE**: Makes the timer the currently displayed app.
     - **\_START**: Start the timer.
@@ -169,7 +169,7 @@ Some apps have additional commands and these are detailed below.
 - \_RADIO APP
     - **\_ACTIVATE**: Makes the radio the currently displayed app.
     - **\_PLAY**: Start the radio.
-    - **\_PLAY CHANNEL**: Start a specific raidio station.
+    - **\_PLAY CHANNEL**: Start a specific radio station.
         - *\_INDEX*: The station to play.
     - **\_STOP**: Stop the radio.
     - **\_NEXT**: Next radio station.
@@ -182,26 +182,27 @@ Some apps have additional commands and these are detailed below.
 
 ### Resource States
 
-The TIME resource has the following states which are updated on each poll:
+The Time resource has the following states which are updated on each poll:
 
 - \_LAMETRIC TIME
     - **\_MODE**:
         - *MANUAL*: Click to scroll mode, when user can manually switch between apps.
         - *AUTO*: Auto\-scroll mode, when the device switches between apps automatically.
-        - *SCHEDULE*: Mode when apps get switched according to a schedule.
+        - *SCHEDULE*: Mode when apps get switched according to a schedule defined using the smartphone app.
         - *KIOSK*: Kiosk mode when single app is locked on the device.
-    - **\_VOLUME**: The current volums set on the device.
-    - **\_SCREENSAVER ENABLED**: Indicates whether the screen save is allowed to operate.
+    - **\_VOLUME**: The current volume set on the device.
+    - **\_SCREENSAVER ENABLED**: Indicates whether the screensaver is allowed to activate. When the screensaver is active activating
+    an app will not work.
 
 All the app resources simply expose a single state which indicates whether or not the app if currently visible
 on the device. Only one app can be visible at a time and the state is only updated on polling.
 
 - \_CLOCK APP, \_TIMER APP, \_STOPWATCH APP, \_RADIO APP, \_WEATHER APP and \_3RD PARTY APP
-    - **\_VISIBLE**: Indicates whether the app is currently displayed on the TIME device.
+    - **\_VISIBLE**: Indicates whether the app is currently displayed on the Time device.
 
 ### Driver Compatibility
 
-This driver is tested against LaMeteic TIME firmware **v2.3.0**, but should be fully compatible with **v2.1.0**.
+This driver is tested against LaMetric Time firmware **v2.3.0**, but should be fully compatible with **v2.1.0**.
 
 For the older **v2.0**, the driver should still work but with limited functionality. You should expect the *\_LAMETRIC TIME*
 notifications and most settings to work, but all apps  and screensaver settings are not supported in this version.
@@ -367,7 +368,7 @@ resource_types = {
         arguments = {
           intArgument("_INDEX", 1, 1, 10)
         },
-        context_help = "Start a specific raidio station."
+        context_help = "Start a specific radio station."
       },
       ["_STOP"] = {
         arguments = {
